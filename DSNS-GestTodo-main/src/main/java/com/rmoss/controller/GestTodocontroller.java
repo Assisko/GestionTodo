57b6c0e2b133b00f40060409ef20b68ebda028c5
Assiko <syllah313@gmail.com>
2024-12-21 11:59:39 +0100

    Try introduce MVC structure in GestionTodo

---
+++ DSNS-GestTodo-main/src/main/java/com/rmoss/controller/GestTodocontroller.java
@@ -0,0 +1,25 @@
+package com.rmoss.controller;
+import com.rmoss.model.GestTodoModel;
+import com.rmoss.view.GestTodoview;
+
+public class GestTodocontroller {
+
+    // Declaration des propriétés
+
+    private final GestTodoModel modele;
+    private final GestTodoview vu;
+
+// constructeur de la class GestTodocontroller
+
+    public GestTodocontroller(GestTodoModel modele, GestTodoview vu) {
+        this.modele = modele;
+        this.vu = vu;
+    }
+// Affichage des details des taches
+
+    public void start(){
+
+        boolean run = true;
+    }
+
+}
